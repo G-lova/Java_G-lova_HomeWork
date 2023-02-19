45f473af1c4343e2c74ef7bd1de1ebb95c930ba7
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2023-02-19 13:41:20 +0300

    task 1 solved

--- Seminar001/Task001.java
+++ Seminar001/Task001.java
@@ -2,27 +2,34 @@ import java.util.Scanner;
 
 /**
  * Task001
- * Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+ * Вычислить n-ое треугольного число(сумма чисел от 1 до n),
  * а так же n! (произведение чисел от 1 до n)
  */
 
 public class Task001 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.printf("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Введите число больше 0: ");
         int n = sc.nextInt();
+        if (n <= 0) {
+            System.out.printf("Введите число больше 0: ");
+            n = sc.nextInt();
+        }
         sc.close();
         System.out.printf("Сумма чисел от 1 до %d = %d\n", n, sumN(n));
         System.out.printf("Факториал %d = %d\n", n, factorialN(n));
+        
     }
 
     public static int sumN(int n) {
-        if (n == 1) return 1;
+        if (n == 1)
+            return 1;
         return n + sumN(n - 1);
     }
 
-    public static int factorialN (int n) {
-        if (n == 1) return 1;
+    public static int factorialN(int n) {
+        if (n == 1)
+            return 1;
         return n * factorialN(n - 1);
     }
 }
